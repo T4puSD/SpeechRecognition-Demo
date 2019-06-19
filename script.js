92c42bfe610ffc76298aca63bf86890537aa9d1f
T4puSD <sdtapusd20@gmail.com>
2019-06-19 11:25:40 +0600

    dialogflow adeed succesfully

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var accessToken = "01fa1653f2a349cdae1a10b15827b773";
+var baseUrl = "https://api.api.ai/v1/";
+
 function speechRecognize() {
   var result = document.getElementById("result");
   var buttonImg = document.querySelector("img");
@@ -36,22 +39,62 @@ function speechRecognize() {
       speechToText = event.results[0][0].transcript;
       console.log(speechToText);
       result.innerHTML = speechToText;
-
-      readOutLoud(speechToText);
+      send(speechToText);
+      //readOutLoud(speechToText);
     };
 
     function readOutLoud(message) {
+      //var sendmessage = messsage;
       var speech = new SpeechSynthesisUtterance(message);
+      // var svoice = new window.SpeechSynthesisVoice();
+      // svoice.name = "Microsoft Zira Desktop - English (United States)";
+      // svoice.voiceURI = "Microsoft Zira Desktop - English (United States)";
+      // svoice.lang = "en-US";
+      // speech.voice = svoice;
+
       speech.lang = "en-US";
-      speech.rate = 0.9;
+      speech.rate = 1;
       speech.pitch = 1; // 1 by default
 
       var synth = window.speechSynthesis;
       var voices = synth.getVoices();
       //console.log(voices);
-      speech.voice = voices[4];
+      //speech.voice = voice[4];
+      console.log(voices);
 
       synth.speak(speech);
+    }
+
+    function send(message) {
+      var text = message;
+      $.ajax({
+        type: "POST",
+        url: baseUrl + "query?v=20150910",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+          Authorization: "Bearer " + accessToken
+        },
+        data: JSON.stringify({
+          query: text,
+          lang: "en",
+          sessionId: "somerandomthing"
+        }),
+        success: function(data) {
+          var jresult = data.result.fulfillment.speech;
+          //result.innerHTML = jresult;
+          readOutLoud(jresult);
+          setResponse(jresult);
+        },
+        error: function() {
+          setResponse("Internal Server Error");
+        }
+      });
+      setResponse("Loading...");
+    }
+
+    function setResponse(val) {
+      console.log(val);
     }
   } else {
     // speech recognition API not supported
